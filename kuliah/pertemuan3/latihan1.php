2be844ded172f15f6f166f0f59e01133c0bccc05
Agung <agungmamez@gmail.com>
2022-02-25 16:57:52 +0700

    Menyelesaikan latihan pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,26 @@
+<?php
+//pengulangan
+//for
+//while
+//do.. while
+//foreach : pengulangan khusus array
+
+for ( $i = 0; $i < 5; $i++ ) {
+    echo "Hai semuanya! <br> ";
+}
+
+$i = 0;
+while( $i < 5 ) {
+    echo "Hai Semuanya! <br>";
+$i++;
+
+}
+
+$i = 10;
+do {
+    echo "Hai Semuanya! <br>";
+$i++;
+
+} while( $i < 5 );
+
+?>
